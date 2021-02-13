cec6f1ced3158685652cf2cd992cbbf9d689e2db
tomoyanp <tomoyanpy@gmail.com>
2021-02-13 02:17:05 +0000

    modi

--- generate-log.sh
+++ generate-log.sh
@@ -1,2 +1,3 @@
-cat /var/log/nginx/access.log | kataribe -conf kataribe.toml  > kataribe_`date "+%Y-%m-%d-%H-%M-%S"`.log
+# cat /var/log/nginx/access.log | kataribe -conf kataribe.toml  > kataribe_`date "+%Y-%m-%d-%H-%M-%S"`.log
+sudo alp -f /var/log/nginx/access.log | kataribe -conf kataribe.toml  > kataribe_`date "+%Y-%m-%d-%H-%M-%S"`.log
 sudo pt-query-digest /var/log/mysql/mysql-slow.sql > pt-query-digest_`date "+%Y-%m-%d-%H-%M-%S"`.log
